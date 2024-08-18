16608bd8589eb0ca845dfa48031b5ea3bdd6a8a9
Andrew Salko <andrewsalko@gmail.com>
2024-08-18 15:28:18 +0300

    enemy bullet added (without animation)

--- SpaceShooter/enemy.gd
+++ SpaceShooter/enemy.gd
@@ -3,6 +3,8 @@ extends Area2D
 var start_pos = Vector2.ZERO
 var speed = 0
 
+var bullet_scene = preload("res://enemy_bullet.tscn")
+
 @onready var screensize  = get_viewport_rect().size
 
 signal died
@@ -24,7 +26,6 @@ func start(pos):
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	position.y += speed * delta
@@ -38,6 +39,9 @@ func _on_move_timer_timeout():
 
 
 func _on_shoot_timer_timeout():
+	var b = bullet_scene.instantiate()
+	get_tree().root.add_child(b)
+	b.start(position)
 	$ShootTimer.wait_time = randf_range(4, 20)
 	$ShootTimer.start()
 
